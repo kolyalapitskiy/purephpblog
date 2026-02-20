8fb786d3b72a7a7241a25c1ef32bd1418d4d35ff
kolyalapitskiy <kolyalapitskiy@gmail.com>
2026-02-20 16:00:46 +0500

    added some changes

--- core/database.php
+++ core/database.php
@@ -1,14 +1,16 @@
 <?php
+// Класс подключения к базе данных, класс используется потому
 class Database {
-    private static $connection;
-    public function __construct($config) {
+    private $connection; //
+
+    public function __construct($config) { // __construct это функция которая вызовет сама себя при запуске, из-за этого тут нет имени.
         $dsn = "mysql:host=" . $config['host'] . ";dbname=" . $config['dbname'];
-        $this->connection = new PDO($dsn, 'root', '', [
+        $this->connection = new PDO($dsn, 'root', '', [ // тут мы обращаемся к $this->connection Для того чтобы перезаписать состояние этой переменной, а new PDO, это класс из-за этого new, а его название PDO.
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ]);
     }
 
-    public function query() {
+    public function query() { // функция sql запрос
         $stmt = $this->connection->prepare("SELECT * FROM posts ORDER By id");
         $stmt->execute();
         $blogs = $stmt->fetchAll(PDO::FETCH_ASSOC);
